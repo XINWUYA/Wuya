664f953e733d7b388f109cc85d9989c2972b5107
xinwuya <xinwuya_scu@163.com>
2021-11-16 02:39:54 +0800

    测试Event事件

--- Kernel/src/Wuya/Core/Logger.h
+++ Kernel/src/Wuya/Core/Logger.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Wuya/Common/Utils.h"
-#include "spdlog/spdlog.h"
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
 
 namespace Wuya 
 {
@@ -22,12 +23,16 @@ namespace Wuya
 }
 
 // Related macros to simplify
-#define CORE_LOG(...)			::Wuya::Logger::GetCoreLogger()->trace(__VA_ARGS__)
-#define CORE_LOG_INFO(...)		::Wuya::Logger::GetCoreLogger()->info(__VA_ARGS__)
-#define CORE_LOG_WARN(...)		::Wuya::Logger::GetCoreLogger()->warn(__VA_ARGS__)
-#define CORE_LOG_ERROR(...)		::Wuya::Logger::GetCoreLogger()->error(__VA_ARGS__)
+#define CORE_LOG(...)				::Wuya::Logger::GetCoreLogger()->trace(__VA_ARGS__)
+#define CORE_LOG_INFO(...)			::Wuya::Logger::GetCoreLogger()->info(__VA_ARGS__)
+#define CORE_LOG_WARN(...)			::Wuya::Logger::GetCoreLogger()->warn(__VA_ARGS__)
+#define CORE_LOG_ERROR(...)			::Wuya::Logger::GetCoreLogger()->error(__VA_ARGS__)
+#define CORE_LOG_DEBUG(...)			::Wuya::Logger::GetCoreLogger()->debug(__VA_ARGS__)
+#define CORE_LOG_CRITICAL(...)		::Wuya::Logger::GetCoreLogger()->critical(__VA_ARGS__)
 
-#define CLIENT_LOG(...)			::Wuya::Logger::GetClientLogger()->trace(__VA_ARGS__)
-#define CLIENT_LOG_INFO(...)	::Wuya::Logger::GetClientLogger()->info(__VA_ARGS__)
-#define CLIENT_LOG_WARN(...)	::Wuya::Logger::GetClientLogger()->warn(__VA_ARGS__)
-#define CLIENT_LOG_ERROR(...)	::Wuya::Logger::GetClientLogger()->error(__VA_ARGS__)
+#define CLIENT_LOG(...)				::Wuya::Logger::GetClientLogger()->trace(__VA_ARGS__)
+#define CLIENT_LOG_INFO(...)		::Wuya::Logger::GetClientLogger()->info(__VA_ARGS__)
+#define CLIENT_LOG_WARN(...)		::Wuya::Logger::GetClientLogger()->warn(__VA_ARGS__)
+#define CLIENT_LOG_ERROR(...)		::Wuya::Logger::GetClientLogger()->error(__VA_ARGS__)
+#define CLIENT_LOG_DEBUG(...)		::Wuya::Logger::GetClientLogger()->debug(__VA_ARGS__)
+#define CLIENT_LOG_CRITICAL(...)	::Wuya::Logger::GetClientLogger()->critical(__VA_ARGS__)
--- Terminator/src/TerminatorApp.cpp
+++ Terminator/src/TerminatorApp.cpp
@@ -1,8 +1,17 @@
 #include "TerminatorApp.h"
 /* 程序主入口点，需要保证放在Wuya.h之后 */
 #include <Wuya/Application/EntryPoint.h>
+#include <Wuya/Events/ApplicationEvent.h>
 
 void TerminatorApp::Run()
 {
 	CLIENT_LOG_WARN("Hello! {0}", 5);
+
+	Wuya::WindowResizeEvent e(1280, 720);
+
+	if (e.IsInCategory(Wuya::EventCategoryApplication))
+	{
+		CLIENT_LOG_DEBUG(e.GetName());
+		CLIENT_LOG_CRITICAL(e);
+	}
 }
